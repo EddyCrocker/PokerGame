8108be12a8414a8f15f67fb87aeebe9ee66a950f
eddy-crocker <54315932+eddy-crocker@users.noreply.github.com>
2019-10-01 11:19:26 +0200

    Project :D

---
+++ src/main/java/PokerGame/PokerGame/PokerGameApplication.java
@@ -0,0 +1,13 @@
+package PokerGame.PokerGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerGameApplication.class, args);
+	}
+		private int ciao = 0;
+}
